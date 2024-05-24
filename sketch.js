b9e3afffd0c8bdeaf963eec9204683891c2e93b3
Roshni <162877854+rbha917@users.noreply.github.com>
2024-05-24 22:47:45 +1200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ let checkbox, checkbox2;
 let hays;
 let carrots;
 let left, right, up, down;
+let hay;
+let carrot;
 
 function preload(){
   mix1 = loadImage ('images/mix1.png');
@@ -33,11 +35,11 @@ function setup() {
 function intro() {
   image(field, 0, 0, 1200, 600);
   image(horse, 250, 150, 500, 400);
-  let hay = createButton('Eat Hay');
+  hay = createButton('Eat Hay');
   hay.position(800, 500);
   hay.size(100, 50);
 	hay.mousePressed(snowing);
-  let carrot = createButton('Eat Carrots');
+  carrot = createButton('Eat Carrots');
   carrot.position(950, 500);
   carrot.size(100, 50);
   carrot.mousePressed(scorching);
@@ -51,16 +53,18 @@ function intro() {
 }
 
 function scorching (){
+  hay.remove();
+  carrot.remove();
   image(desert, 0, 0, 1200, 600);
   image(horse, 250, 150, 500, 400);
-  let up = createButton('Jump Up');
+  up = createButton('Jump Up');
   up.position(200, 500);
   up.size(100, 50);
 	up.mousePressed(sunmix);
-  let down = createButton('Sink Down');
+  down = createButton('Sink Down');
   down.position(800, 500);
   down.size(100, 50);
-	down.mousePressed(end);
+	down.mousePressed(end2);
   fill(255);
   noStroke();
   rect(130, 85, 750, 20)
@@ -70,16 +74,18 @@ function scorching (){
 }
 
 function snowing (){
+  hay.remove();
+  carrot.remove();
   image(snow, 0, 0, 1200, 600);
   image(horse, 250, 150, 500, 400);
-  let left = createButton('Go Left');
+  left = createButton('Go Left');
   left.position(200, 500);
   left.size(100, 50);
 	left.mousePressed(snowmix);
-  let right = createButton('Go Right');
+  right = createButton('Go Right');
   right.position(800, 500);
   right.size(100, 50);
-	right.mousePressed(end);
+	right.mousePressed(end1);
   fill(255);
   noStroke();
   rect(175, 85, 650, 20)
@@ -88,6 +94,8 @@ function snowing (){
 }
 
 function snowmix (){
+  left.remove();
+  right.remove();
   image(mix2, 0, 0, 400, 600);
   image(mix2, 400, 0, 400, 600);
   image(mix2, 800, 0, 400, 600);
@@ -100,11 +108,13 @@ function snowmix (){
   checkbox = createCheckbox();
   checkbox.position(350, 500);
   if (checkbox.checked()){
-    end();
+    end3();
   }
 }
 
 function sunmix (){
+  up.remove();
+  down.remove();
   image(mix1, 0, 0, 400, 600);
   image(mix1, 400, 0, 400, 600);
   image(mix1, 800, 0, 400, 600);
@@ -117,20 +127,55 @@ function sunmix (){
   checkbox2 = createCheckbox();
   checkbox2.position(900, 500);
   if (checkbox2.checked()){
-    end();
+    end4();
   }
 }
 
-function end() {
+function end1() {
   image(field, 0, 0, 1200, 600);
   image(horse, 250, 150, 500, 400);
   fill(255);
   noStroke();
   rect(20, 85, 960, 20)
   fill(0);
-  text('Congrats, you made it back to field, let this be a lesson not to eat anything you see in your path', 500, 100);
+  text('Congrats, you made it back to field, let this be a lesson not to eat whatever you see in your path', 500, 100);
+  right.remove();
+  left.remove();
+  up.remove();
+  down.remove();
 }
 
-function draw() {
-  
+function end2() {
+  image(field, 0, 0, 1200, 600);
+  image(horse, 250, 150, 500, 400);
+  fill(255);
+  noStroke();
+  rect(20, 85, 960, 20)
+  fill(0);
+  text('Congrats, you made it back to field, let this be a lesson not to eat whatever you see in your path', 500, 100);
+  up.remove();
+  down.remove();
 }
+
+function end3(){
+  image(field, 0, 0, 1200, 600);
+  image(horse, 250, 150, 500, 400);
+  fill(255);
+  noStroke();
+  rect(20, 85, 960, 20)
+  fill(0);
+  text('Congrats, you made it back to field, let this be a lesson not to eat whatever you see in your path', 500, 100);
+  checkbox.remove();
+}
+
+function end4() {
+  image(field, 0, 0, 1200, 600);
+  image(horse, 250, 150, 500, 400);
+  fill(255);
+  noStroke();
+  rect(20, 85, 960, 20)
+  fill(0);
+  text('Congrats, you made it back to field, let this be a lesson not to eat whatever you see in your path', 500, 100);
+  checkbox2.remove();
+}
+
